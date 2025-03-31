46dae185a54d8e74ccb9d549c6e3f044b2093c5c
Göktuğ Gezer <ggezer780@gmail.com>
2025-03-31 21:54:49 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,14 @@ const nextConfig = {
   images: {
     domains: ["i.pravatar.cc","firebasestorage.googleapis.com"],
   },
+  //! CORS Settings
   async headers() {
     return [
       {
         source:"/api/:path*",
         headers:[
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
+          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_BASE_URL }, 
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
